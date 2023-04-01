bde9cd6d4e62da7a3508dc9c23da680c5c8b2990
Plamen Stoilkov <109562327+sdplamen@users.noreply.github.com>
2023-04-01 18:21:25 +0300

    Create convert_km_miles.js

---
+++ convert_km_miles.js
@@ -0,0 +1,13 @@
+const miles = 24;
+const kilometers = 40;
+const number = 1.609344;
+const kilometersToMiles = kilometers / number;
+const milesToKilometers = miles * number;
+console.log("Distance conversions");
+console.log("-----------------------");
+console.log(kilometers + " kilometers are:");
+console.log(kilometersToMiles + " miles");
+console.log("----");
+console.log(miles + " miles are:");
+console.log(milesToKilometers + " kilometers");
+console.log("\n");
