5d86c64a6f0aa95ab7add1da5c00f2f6352b5a32
Plamen Stoilkov <109562327+sdplamen@users.noreply.github.com>
2023-04-01 18:22:38 +0300

    Create convert_pound_ounce.js

---
+++ convert_pound_ounce.js
@@ -0,0 +1,7 @@
+function poundsToOunces (pounds) {
+  const conversion = 16;
+  const output = pounds * conversion;
+  return output + " Ounces";
+}
+const result = poundsToOunces(6);
+console.log(result);
